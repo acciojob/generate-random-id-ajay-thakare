9de89ba86a12b3615e414524749c66eeeafa3f75
Ajay Thakare <143386761+ajay-thakare@users.noreply.github.com>
2026-06-05 09:18:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 function makeid(l) {
   // write your code here
+
+	let listChar = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcedefghijklmnopqrstuvwxyz0123456789';
+	let result = '';
+	for(let i = 1; i <= l; i++){
+		result += listChar.charAt(
+			Math.floor(
+				(Math.random() * listChar.length);
+			)
+		)
+	}
+
+	return result;
 }
 
 // Do not change the code below.
